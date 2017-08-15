b27ec8b8d28fe539256a331619954c4fe66b1e2c
monlor <monlor@monlor-mac.local>
2017-08-15 10:11:41 +0800

    add all files

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 [ -f $1 ] && echo "Please input install place!" && exit
 [ -d $1/monlor ] && echo "$1/monlor directory exist!" && exit
+[ `cat /proc/xiaoqiang/model` != "R2D" ] && echo "Only support Miwifi R2D!" && exit
+echo "Sure to install Monlor-Tools?(y/n)"
+read ans
+[ "$ans" != "y" ] && exit 
 curl -Lo /tmp/monlor.zip https://coding.net/u/monlor/p/Monlor-Tools/git/raw/master/monlor.zip
 mkdir $1/monlor
 unzip /tmp/monlor.zip -d $1/monlor
