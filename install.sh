d4a8848dbc8e42b4a0eea49259d1a8eda35c8557
monlor <monlor@monlor-mac.local>
2017-08-17 20:48:31 +0800

    auto sync

--- install.sh
+++ install.sh
@@ -7,9 +7,10 @@ clear
 echo -n "Sure to install Monlor-Tools?(y/n) "
 read ans
 [ "$ans" != "y" ] && exit 
-curl -Lo /tmp/monlor.zip https://coding.net/u/monlor/p/Monlor-Tools/git/raw/master/monlor.zip
+rm -rf /tmp/monlor.zip
+curl -Lo /tmp/monlor.zip https://coding.net/u/monlor/p/Monlor-Tools/git/raw/master/app/monlor.zip
 mkdir $1/monlor
-unzip /tmp/monlor.zip -d $1/monlor
+unzip /tmp/monlor.zip -d $1/monlor || exit
 chmod -R +x $1/monlor
 $1/monlor/script/allrun install $1/monlor
 rm -rf /tmp/monlor.zip
