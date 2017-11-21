a22c01db29ce33c81964dbde258c6bd52e842dfb
monlor <monlor@qq.com>
2017-11-21 21:10:30 +0800

    2.0

--- scripts/keepalive.sh
+++ scripts/keepalive.sh
@@ -4,7 +4,7 @@ source base.sh
 while(true) 
 do
 	sleep 60
-	result=$(ps | grep monitor.sh | wc -l)
+	result=$(ps | grep monitor.sh | grep -v grep | wc -l)
 	if [ "$result" == '0' ]; then
 		$monlorpath/scripts/monitor.sh > /dev/null 2>&1
 	fi
